61ecbb77fdb286f5ea059025f492247ede000ae4
Jason <jasonli1337@gmail.com>
2022-08-25 18:42:26 -0400

    Commentary commit

--- Assets/JasonLi/TestObject.cs
+++ Assets/JasonLi/TestObject.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using MunroHoberman;
 
+// Make sure to include your namespace!
 namespace JasonLi
 {
     public class TestObject : MonoBehaviour
     {
         public MunroHoberman.TestObject testObject;
-
+        
 
         void Start()
         {
