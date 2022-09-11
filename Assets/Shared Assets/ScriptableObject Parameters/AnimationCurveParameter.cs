a8e4df417531eb0bc9b5fbc0daace5ee7bfa72b5
Jason Li <jasonli1337@gmail.com>
2022-09-11 19:23:21 -0400

    Add Parameter Scriptable Objects to Shared Assets
    Float, Int, String, and Animation Curve

---
+++ Assets/Shared Assets/ScriptableObject Parameters/AnimationCurveParameter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SharedAssets
+{
+    [CreateAssetMenu(fileName = "New AnimationCurve Parameter", menuName = "Parameter Object/AnimationCurve Parameter")]
+    public class AnimationCurveParameter : ScriptableObject
+    {
+        [SerializeField] private AnimationCurve _curve;
+
+        public float Evaluate(float t) => _curve.Evaluate(t);
+
+    }
+}
