c76bc9595ba901a8b69e9193be97d4afb4d31147
gamesbyaisha <nekoh150@newschool.edu>
2022-09-12 17:06:44 -0400

    Game works now

---
+++ Assets/AishaBikebayeva/Arcade Game/Scripts/StonesSpawn.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StonesSpawn: MonoBehaviour
+{
+    public GameObject Prefab;
+    public float timeBetweenSpawns; //interval of time to wait between spawn in seconds
+
+    public float ySpawnPosMin; //up most spawn point
+    public float ySpawnPosMax; //down most spawn point
+    public float xSpawnPos; //width of spawn
+
+    //private variables can't be accessed by other scripts
+    private float timeUntilSpawn;
+
+    public void Start()
+    {
+        timeUntilSpawn = 1;
+    }
+
+    public void Update()
+    {
+        //Time.deltaTime is how much time has occured since the last update. 
+        //If we decrease the timer by Time.deltaTime every time Update() runs, the timer will decrease by 1.0 every second.
+        timeUntilSpawn -= Time.deltaTime;
+        //Once timeUntilSpawn is less than 0, we spawn a new hat
+        if (timeUntilSpawn <= 0)
+        {
+            Spawn();
+            //then we reset timeUntilSpawn to the timeBetweenSpawns & start all over again
+            timeUntilSpawn = timeBetweenSpawns;
+        }
+    }
+
+    private void Spawn()
+    {
+        //Generate a new spawn position. 
+        //For the first value of this Vector3 (x) we use Random.Range to get a position between our min & max X values
+        //The second (y) is just the height we spawn at
+        //The third (z) is the depth, which is set to 0 as we're in 2D
+        Vector3 newPos = new Vector3(xSpawnPos, Random.Range(ySpawnPosMin, ySpawnPosMax), 0);
+        //Instantiate creates a new object from a prefab. It needs the prefab, the position, and the rotation as arguements
+        //newPos is the position we made, Quaternion.identity just means that we're not rotating the sprite
+        Instantiate(Prefab, newPos, Quaternion.identity);
+    }
+
+
+
+}
---
+++ Assets/AishaBikebayeva/Arcade Game/Scripts/gameOverScreen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gameOverScreen : MonoBehaviour
+{
+    public Text pointsText;
+
+    public void SetUp (int score)
+    {
+        gameObject.SetActive(true);
+        pointsText.text = score.ToString() + " POINTS";
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
