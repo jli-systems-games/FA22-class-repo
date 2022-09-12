2bc21f77b167b283ef8cee78fffdd47783ddb12b
zaksaja <81482722+zaksaja@users.noreply.github.com>
2022-09-12 17:35:19 -0400

    Added project1 maze

---
+++ Assets/samantha zak/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       // if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+       // {
+       //     this.movement
+       // }
+    }
+}
