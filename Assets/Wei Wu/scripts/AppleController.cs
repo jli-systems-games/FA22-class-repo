b2f635811919f6d025b5324a0fd7f9c65d17397a
Weeeeeeeeei <wei1009760731@gmail.com>
2022-09-12 14:08:22 -0700

    eh

--- Assets/Wei Wu/scripts/AppleController.cs
+++ Assets/Wei Wu/scripts/AppleController.cs
@@ -6,7 +6,10 @@ public class AppleController : MonoBehaviour
 {
 
     public float speed;
-   
+    public GameObject collectedEffect;
+
+    private SpriteRenderer _spriteRenderer;
+    private BoxCollider2D _boxCollider2D;
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +24,29 @@ public class AppleController : MonoBehaviour
         transform.Translate(Vector3.down * Time.deltaTime * speed, Space.Self); 
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _spriteRenderer.enabled = false;
+            _boxCollider2D.enabled = false;
 
-//private void OnTriggerEnter(Collider col)
-//{
- //   if (col.name == "Head")
- //   {
-  //      snake.GetComponent<SnakeController>().getApple();
-//    }
- //   else if(col.name =="Body Clone")
-//    {
+            collectedEffect.SetActive(true);
 
-//    }
-//}
+            Destroy(gameObject, 2f);
+        }
+    }
+    //private void OnTriggerEnter(Collider col)
+    //{
+    //   if (col.name == "Head")
+    //   {
+    //      snake.GetComponent<SnakeController>().getApple();
+    //    }
+    //   else if(col.name =="Body Clone")
+    //    {
+
+    //    }
+    //}
 
 
 
