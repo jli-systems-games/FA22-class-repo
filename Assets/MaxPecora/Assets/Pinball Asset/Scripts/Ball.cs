a118de12d9c0e83ee9f0d6e707eccc619c7bfc40
pecom291 <pecom291@newschool.edu>
2022-09-12 15:52:48 -0400

    prototype
    - basic outline
    - crying in spanish

---
+++ Assets/MaxPecora/Assets/Pinball Asset/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
+    }    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Spikes")
+        {
+            gameManager.DestroyBall(gameObject);
+        }
+    }
+}
