5eca4fb164c5ec086475b64cc565ef1f0f350574
pecom291 <pecom291@newschool.edu>
2022-09-12 16:42:11 -0400

    Added Project 1
    - the entirety of the prototype for my first project

---
+++ Assets/MaxPecora/Assets/Pinball Asset/Scripts/Bouncer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+    [SerializeField]
+    private Animation hitAnim;
+
+    [SerializeField]
+    private SoundManager soundManager;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            soundManager.BouncerHit();
+            hitAnim.Play();
+        }
+    }
+}
