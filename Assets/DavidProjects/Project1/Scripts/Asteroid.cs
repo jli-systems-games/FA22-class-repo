de2309eccae2ceef91e5451f54b945ee8bbbf0a0
David <duy312@newschool.edu>
2022-09-16 16:39:00 -0400

    Edit project1

---
+++ Assets/DavidProjects/Project1/Scripts/Asteroid.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Asteroid : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Sprite[] sprites;
+
+    public float size = 1f;
+    public float minSize = 0.35f;
+    public float maxSize = 1.65f;
+    public float movementSpeed = 50f;
+    public float maxLifetime = 30f;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+        transform.eulerAngles = new Vector3(0f, 0f, Random.value * 360f);
+
+        transform.localScale = Vector3.one * size;
+        rigidbody.mass = size;
+
+        Destroy(gameObject, maxLifetime);
+    }
+
+    public void SetTrajectory(Vector2 direction)
+    {
+        rigidbody.AddForce(direction * movementSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            if ((size * 0.5f) >= minSize)
+            {
+                CreateSplit();
+                CreateSplit();
+            }
+            FindObjectOfType<GameManager>().AsteroidDestroyed(this);
+            Destroy(gameObject);
+        }
+    }
+
+    private Asteroid CreateSplit()
+    {
+        Vector2 position = transform.position;
+        position += Random.insideUnitCircle * 0.5f;
+
+        Asteroid half = Instantiate(this, position, transform.rotation);
+        half.size = size * 0.5f;
+
+        half.SetTrajectory(Random.insideUnitCircle.normalized);
+
+        return half;
+    }
+
+}
---
+++ Assets/DavidProjects/Project1/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    public float speed = 500f;
+    public float maxLifetime = 10f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Project(Vector2 direction)
+    {
+        rigidbody.AddForce(direction * speed);
+
+        Destroy(gameObject, maxLifetime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
