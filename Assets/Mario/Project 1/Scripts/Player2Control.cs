a9d5e12fbb983797e37ddff65337339b8e65d389
MarioBrnjac <90148758+MarioBrnjac@users.noreply.github.com>
2022-09-17 17:59:18 -0400

    Added polish and edited scripts

--- Assets/Mario/Project 1/Scripts/Player2Control.cs
+++ Assets/Mario/Project 1/Scripts/Player2Control.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MarioAirHockey
+namespace MarioArcadeSoccer
 {
 
     public class Player2Control : MonoBehaviour
--- Assets/Mario/Project 1/Scripts/PlayerControl.cs
+++ Assets/Mario/Project 1/Scripts/PlayerControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MarioAirHockey
+namespace MarioArcadeSoccer
 {
 
     public class PlayerControl : MonoBehaviour
