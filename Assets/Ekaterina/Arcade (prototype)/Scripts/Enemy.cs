5f2a66a3af5478c5201c9d64f84844e3f03dff23
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-09-18 16:00:30 -0400

    Enemy added

---
+++ Assets/Ekaterina/Arcade (prototype)/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float dirX;
+    private float moveSpeed;
+    private Rigidbody2D rb;
+    private bool facingRight = false;
+    private Vector3 localScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localScale = transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+        dirX = -1f;
+        moveSpeed = 3f;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Wall>())
+        {
+            dirX *= -1f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+    }
+
+    void LateUpdate()
+    {
+        CheckWhereToFace();
+    }
+
+    void CheckWhereToFace()
+    {
+        if (dirX > 0)
+            facingRight = true;
+        else if (dirX < 0)
+            facingRight = false;
+
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+            localScale.x *= -1;
+
+        transform.localScale = localScale;
+    }
+}
