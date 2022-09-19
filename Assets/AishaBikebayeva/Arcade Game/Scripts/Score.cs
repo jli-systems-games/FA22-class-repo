ce76affb81e1d713dcfbcb0b4acaa296f0c87ec4
gamesbyaisha <nekoh150@newschool.edu>
2022-09-18 23:57:31 -0400

    Replaced "play again" button with space key to restart game

--- Assets/AishaBikebayeva/Arcade Game/Scripts/Score.cs
+++ Assets/AishaBikebayeva/Arcade Game/Scripts/Score.cs
@@ -14,13 +14,13 @@ namespace Aisha
         //private SpriteRenderer sr;
 
         //private variables have to be set in code
-        private int score;
+        public int score;
     
 
         //Start is called at game start, sets the score to 0
         void Start()
         {
-            score = 0;
+            float score = 0;
         }
 
         // Update is called once per frame
@@ -29,25 +29,20 @@ namespace Aisha
             scoreText.text = "Score: " + score; 
             //score += Time.deltaTime;                                         
             score += 1;                                                             
-                                                                        
-                                                                            
+                                                                      
         }
 
-        public void StoneHit()
-        {
-            Debug.Log("player died"); //Print a useful message in the debug console
-            score -= 0;
+        // public void StoneHit()
+        // {
+        //     Debug.Log("player died"); //Print a useful message in the debug console
+        //     score -= 0;
             
-        }
+        // }
 
-        public void StoneMissed()
-        {
-            Debug.Log("player is doing well"); //Print a useful message in the debug console
-            score += 1;
-        }
-
-
+        // public void StoneMissed()
+        // {
+        //     Debug.Log("player is doing well"); //Print a useful message in the debug console
+        //     score += 1;
+        // }
     }
-
-
 }
