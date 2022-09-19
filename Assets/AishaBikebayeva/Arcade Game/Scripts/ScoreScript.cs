879a64432f27e6a3aea7df6dc6057b13b35e4dc8
gamesbyaisha <nekoh150@newschool.edu>
2022-09-18 20:23:10 -0400

    Made logo pixelated and changed sprite and background

---
+++ Assets/AishaBikebayeva/Arcade Game/Scripts/ScoreScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Aisha
+{
+    
+    public class ScoreScript : MonoBehaviour
+    {
+        public static float scoreNum;
+        public Text Feedback;
+        public TextMeshProUGUI WinScore;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // scoreNum = Score.score;
+
+            WinScore.text = "Final Score: " + scoreNum;
+        }
+    }
+
+}
