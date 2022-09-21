2c6c60476c10593cd6a41393628adf6075d93ad3
DinosaurPower <lysym685@newschool.edu>
2022-09-21 17:35:00 -0400

    crime
    made a prototype of sprite and code

--- Assets/MaiiaLysytska/Scripts/Unicode.cs
+++ Assets/MaiiaLysytska/Scripts/Unicode.cs
@@ -1,20 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Unicode : MonoBehaviour
+namespace godzillabanana{
+
+public class Unicode : MonoBehaviour, IEndDragHandler, IDragHandler
 {
+        [SerializeField] private Canvas canvas;
+        public Transform rectTransform;
+        public CanvasGroup canvasgroup;
     public bool isClicked;
     public int poop;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+            rectTransform = GetComponent<RectTransform>();
+            canvasgroup = GetComponent<CanvasGroup>();
+        }
 
     // Update is called once per frame
     void Update()
     {
         poop += 1;
     }
+
+
+        public void OnDrag(PointerEventData eventData)
+        {
+
+        //rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+        public void OnEndDrag(PointerEventData eventData)
+        {
+
+        }
+}
 }
