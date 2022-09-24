52b31907ae7ef70391f67feac30f5feaf34d34f3
DinosaurPower <lysym685@newschool.edu>
2022-09-23 21:39:10 -0400

    sin of MOAR SCRIPTS (and slots)
    added preparation for slot script, added slots, getting ready for unicorn breeding system

---
+++ Assets/MaiiaLysytska/Scripts/Slot.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace godzillabanana { 
+public class Slot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
