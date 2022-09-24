f868543cc608dd65081e9c831561b2b55c34074d
babybabel <schrs973@newschool.edu>
2022-09-24 18:56:05 -0400

    Project 1 Reorganization + Initial Project 2 Setup
    project 2 setup includes initial scripts and sprites

---
+++ Assets/Simon/Project2/Scripts/NoteObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Simon.Project2.Scripts 
+{
+        
+    public class NoteObject : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+    
+}
+
