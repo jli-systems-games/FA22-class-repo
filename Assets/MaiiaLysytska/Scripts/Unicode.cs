1fc0ba1e11fa356e8fc59d21cf6e5fd9114ca9d0
DinosaurPower <lysym685@newschool.edu>
2022-09-25 17:40:23 -0400

    Sin of creating more bugs and mixing colors
    made colormixing mechanic, have no clue how to make instantiate work

--- Assets/MaiiaLysytska/Scripts/Unicode.cs
+++ Assets/MaiiaLysytska/Scripts/Unicode.cs
@@ -9,19 +9,27 @@ namespace godzillabanana
 
     public class Unicode : MonoBehaviour
     {
+        public bool colided = false;
         public Camera mainCamera;
         //public Vector3 mousePos;
         public Drawing drawing;
         public Transform recentPos;
         public Transform ponyPos;
    public TMP_Text text;
-        public int poopValue;
+        public float poopValue;
         public string unicornType;
         private float Timer = 5;
         public float TimerValue;
-        public int poop;
+        public float poop;
         public Color color;
-        
+        public Color FriendColor;
+        public float maxpoop;
+        public Color babyColor;
+
+
+
+        public GameObject unicornGene;
+        public GameObject babyunicorn;
         // Start is called before the first frame update
         void Start()
         {
@@ -38,15 +46,9 @@ namespace godzillabanana
         {
             Vector3 mousePos = Input.mousePosition;
             poop += poopValue;
-            if (Timer<= 0) { 
-           
-                Timer = TimerValue;
-               
-            }
 
-            Timer -= Time.deltaTime;
-
-            text.SetText(poop.ToString());
+            maxpoop = Mathf.Round(poop * 10.0f) * 0.1f;
+            text.SetText(maxpoop.ToString());
           
 
         }
@@ -78,5 +80,30 @@ namespace godzillabanana
 
         }
 
+
+        private void OnMouseUp()
+        {
+            if (colided)
+            {
+                Debug.Log("yay");
+                
+                GameObject babyunicorn = GameObject.Instantiate(unicornGene, Vector3.zero, Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
+                babyunicorn.GetComponent<SpriteRenderer>().color = babyColor;
+
+            }
+        }
+
+
+        private void OnTriggerEnter2D(Collider2D obj)
+        {
+            FriendColor = obj.GetComponent<SpriteRenderer>().color;
+            babyColor = new Color(Mathf.Sqrt(color.r * FriendColor.r), Mathf.Sqrt(color.g * FriendColor.g), Mathf.Sqrt(color.b * FriendColor.b), 1.0f);
+
+
+        }
+        private void OnTriggerExit2D()
+        {
+            colided = false;
+        }
     }
 }
