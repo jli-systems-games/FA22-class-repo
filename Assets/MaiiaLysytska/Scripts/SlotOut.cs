02bc249843a1facde7c7985efb10cfb481aac6f3
DinosaurPower <lysym685@newschool.edu>
2022-09-25 14:46:47 -0400

    sin of making more bugs
    started simple crafting system

--- Assets/MaiiaLysytska/Scripts/SlotOut.cs
+++ Assets/MaiiaLysytska/Scripts/SlotOut.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SlotOut : MonoBehaviour
 {
+
+    public bool unicorn1;
+    public bool unicorn2;
+    public bool unicornChild;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,12 @@ public class SlotOut : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (unicorn1 == true && unicorn2 == true && unicornChild == false)
+        {
+            Debug.Log("aa");
+        }
     }
+
+
+
 }
