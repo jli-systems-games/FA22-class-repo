242c99f84596300c5cd5a311874c8fcc2f66c293
pecom291 <pecom291@newschool.edu>
2022-09-25 16:06:48 -0400

    Important Changes prior to rebase
    - player movement script
    - items that increase and decrease size of player
    - unbelievably basic layout done to test mechanics
    - scripts made for item collection

---
+++ Assets/MaxPecora/Project 2/Advanced Controller/Scripts/ItemCollector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Max
+{
+
+    public class ItemCollector : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Growth"))
+            {
+                Destroy(collision.gameObject);
+                transform.localScale += new Vector3(2, 2, 2);
+            } 
+            if (collision.gameObject.CompareTag("Shrink"))
+            {
+                Destroy(collision.gameObject);
+                transform.localScale += new Vector3(-2, -2, -2);
+            }
+        }
+    }
+}
