20afbca6dbd5a0f2b77b650c7d028e6d2aaddfca
Munro Hoberman <31392444+redmoe@users.noreply.github.com>
2022-09-26 12:59:35 -0400

    script edits

--- Assets/MunroHoberman/IdleGame/IdleManager.cs
+++ Assets/MunroHoberman/IdleGame/IdleManager.cs
@@ -4,29 +4,55 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Munro.IdleGame {
+namespace MunroHoberman.IdleGame
+{
     public class IdleManager : MonoBehaviour
     {
-        public double growAmount=0f;
+        public float growAmount = 0f;
         public TextMeshProUGUI amountText;
-        
+
+        public int planters = 0;
+        public float planterPrice = 0;
+        public float planterBasePrice = 10;
+        public float planterPriceMultiplier = 1.1f;
+        public float planterProduction = .1f;
+
+        public TextMeshProUGUI buildText;
+
         // Start is called before the first frame update
         void Start()
         {
-            
-        }
-    
-        // Update is called once per frame
-        void Update()
-        {
-            
+            planterPrice = planterBasePrice;
+            amountText.text = "0";
+            buildText.text = $"Buy Planter ${planterPrice}";
+            InvokeRepeating("MainLoop", 0f, .1f);
         }
 
+        void MainLoop()
+        {
+            growAmount += planters * planterProduction;
+        }
+
+        void Update()
+        {
+            amountText.text =growAmount.ToString("0");
+        }
+        
         public void IncreaseNumber()
         {
             growAmount += 1;
-            amountText.text = growAmount.ToString();
         }
-        
+
+        public void BuyPlanter()
+        {
+            if (growAmount >= planterPrice)
+            {
+                
+                planters++;
+                growAmount -= planterPrice;
+                planterPrice = Mathf.Floor(planterBasePrice * Mathf.Pow(planterPriceMultiplier,planters));
+                buildText.text = $"Buy Planter ${planterPrice}";
+            }
+        }
     }
 }
