da4b546b14e910e822597beb9ad1cd979bf0966e
Nickel <2511276509@qq.com>
2022-09-26 02:05:45 -0400

    add script

--- Assets/Nickel-Growth/Torch.cs
+++ Assets/Nickel-Growth/Torch.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Torch : MonoBehaviour
 {
-    public static float torchTime = 5;
+    public static float torchTime = 10;
     private float torchTimeLocal;
     public Light torchLight;
     private float speed;
@@ -20,16 +20,25 @@ public class Torch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(torchTime);
+        Debug.Log(torchTime);
         //StartCoroutine(LoadSubtitles());
-        torchLight.intensity = torchTime;
+        
         if (torchTime > 0)
         {
+
             torchTime -= Time.deltaTime;
             
             
         }
-        if (torchTime < 0)
+        if (torchTime > 3)
+        {
+            torchLight.intensity = 1.5f;
+        }
+        else if(torchTime < 3)
+        {
+            torchLight.intensity -= Time.deltaTime*0.5f;
+        }
+        else if (torchTime < 0)
         {
             //Debug.Log("Game Over");
         }
@@ -41,6 +50,7 @@ public class Torch : MonoBehaviour
     //IEnumerator LoadSubtitles()
     //{
     //    yield return new WaitForSeconds(torchTime);
+        
     //    Debug.Log("you die");
     //}
 }
