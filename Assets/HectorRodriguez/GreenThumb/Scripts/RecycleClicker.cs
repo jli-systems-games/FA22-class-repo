d349ac13855bae39d09ada68c0d46d613efef0b7
Hectorgamecreator <rodrh308@newschool.edu>
2022-09-27 10:51:51 -0400

    additional changes in maingame1

--- Assets/HectorRodriguez/GreenThumb/Scripts/RecycleClicker.cs
+++ Assets/HectorRodriguez/GreenThumb/Scripts/RecycleClicker.cs
@@ -7,37 +7,65 @@ using UnityEngine.UI;
 public class RecycleClicker : MonoBehaviour
 {
 public float plasticAmount = 0f;
-public float metalAmount = 0;
+public float metalAmount = 0f;
+    public float electronicAmount = 0f;
+    public float bookAmount = 0f;
+
 public TextMeshProUGUI amountText;
 public TextMeshProUGUI amountText2;
+public TextMeshProUGUI amountText3;
+public TextMeshProUGUI amountText4;
 
-public int plastics = 0;
+    public int plastics = 0;
 public int metals = 0;
+    public int electronics = 0;
+    public int books = 0;
 
-public float metalPrice = 0;
-public float metalBasePrice = 10;
-public float metalPriceMultiplier = 1.1f;
-public float metalProduction = .1f;
+
 
     public float plasticPrice = 0;
 public float plasticBasePrice = 10;
 public float plasticPriceMultiplier = 1.1f;
 public float plasticProduction = .1f;
 
-public TextMeshProUGUI buildText;
+    public float metalPrice = 0;
+    public float metalBasePrice = 10;
+    public float metalPriceMultiplier = 1.1f;
+    public float metalProduction = .1f;
+
+    public float electronicPrice = 0;
+    public float electronicBasePrice = 10;
+    public float electronicPriceMultiplier = 1.1f;
+    public float electronicProduction = .1f;
+
+    public float bookPrice = 0;
+    public float bookBasePrice = 10;
+    public float bookPriceMultiplier = 1.1f;
+    public float bookProduction = .1f;
+
+    public TextMeshProUGUI buildText;
 public TextMeshProUGUI buildText2;
+    public TextMeshProUGUI buildText3;
+    public TextMeshProUGUI buildText4;
 
     // Start is called before the first frame update
     void Start()
         {
          plasticPrice = plasticBasePrice;
         metalPrice = metalBasePrice;
+        electronicPrice = electronicBasePrice;
+        bookPrice = bookBasePrice;
+
 
         amountText.text = "0";
         amountText2.text = "0";
+        amountText3.text = "0";
+        amountText4.text = "0";
 
         buildText.text = $"Buy Plastic Upgrade${plasticPrice}";
         buildText2.text = $"Buy Metal Upgrade${metalPrice}";
+        buildText3.text = $"Buy Electronic Upgrade${electronicPrice}";
+        buildText4.text = $"Buy Book Upgrade${bookPrice}";
         InvokeRepeating("MainLoop", 0f, .1f);
         }
 
@@ -45,12 +73,16 @@ public TextMeshProUGUI buildText2;
         {
             plasticAmount += plastics * plasticProduction;
             metalAmount += metals * metalProduction;
+        electronicAmount += electronics * electronicProduction;
+        bookAmount += books * bookProduction;
     }
 
       void Update()
         {
             amountText.text = plasticAmount.ToString("0");
         amountText2.text = metalAmount.ToString("0");
+        amountText3.text = electronicAmount.ToString("0");
+        amountText4.text = bookAmount.ToString("0");
     }
 
     public void IncreaseNumber()
@@ -64,6 +96,19 @@ public TextMeshProUGUI buildText2;
         
         metalAmount += 1;
     }
+
+    public void IncreaseNumber2()
+    {
+
+        electronicAmount += 1;
+    }
+
+    public void IncreaseNumber3()
+    {
+
+        bookAmount += 1;
+    }
+
 
     public void CollectPlastic()
         {
@@ -88,5 +133,30 @@ public TextMeshProUGUI buildText2;
             buildText2.text = $"Buy Metal Upgrade ${metalPrice}";
         }
     }
+    public void CollectElectronic()
+    {
+        if (electronicAmount >= electronicPrice)
+        {
+
+            electronics++;
+            electronicAmount -= electronicPrice;
+            electronicPrice = Mathf.Floor(electronicBasePrice * Mathf.Pow(electronicPriceMultiplier, electronics));
+            buildText3.text = $"Buy Electronic Upgrade ${electronicPrice}";
+        }
+
+    }
+    public void CollectBook()
+    {
+        if (bookAmount >= bookPrice)
+        {
+
+           books++;
+            bookAmount -= bookPrice;
+            bookPrice = Mathf.Floor(bookBasePrice * Mathf.Pow(bookPriceMultiplier, books));
+            buildText4.text = $"Buy Book Upgrade ${bookPrice}";
+        }
+
+    }
 }
+
 
