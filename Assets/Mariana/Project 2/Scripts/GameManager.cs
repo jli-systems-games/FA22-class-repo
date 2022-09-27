f51ab6f6cf4d0fd71daf2912de6d062f793b28ff
m-holtz <74580132+m-holtz@users.noreply.github.com>
2022-09-26 22:03:48 -0400

    Fixed borders

--- Assets/Mariana/Project 2/Scripts/GameManager.cs
+++ Assets/Mariana/Project 2/Scripts/GameManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GameManager : MonoBehaviour
+namespace Mariana
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public class GameManager : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
