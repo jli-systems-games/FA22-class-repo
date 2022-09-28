36fcd6729e50e459354483c6be66e17c5736cd0d
gamesbyaisha <nekoh150@newschool.edu>
2022-09-27 21:36:59 -0400

    Added sprites and button to change on click, but it does not work yet

---
+++ Assets/AishaBikebayeva/Scripts/ChangeSprite.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AishaGrowth
+{
+    // [Serializable]
+    public class ChangeSprite : MonoBehaviour
+    {
+        public Button Button;
+        int clicks = 0;
+        public List<Sprite> sprites;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+        private void OnButtonClick()
+        {
+        GetComponent<SpriteRenderer>().sprite = sprites[clicks];
+        clicks++;
+        // clicks = clicks % sprites.length; 
+        //to ensure the clicks dont go over your sprite count
+        }
+
+        void Awake()
+        {
+            Button.onClick.AddListener(OnButtonClick);
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
