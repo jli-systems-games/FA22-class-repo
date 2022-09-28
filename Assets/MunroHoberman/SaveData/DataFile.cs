30a02776195bd77f94b6bd92e768e2386b50e457
Munro Hoberman <31392444+redmoe@users.noreply.github.com>
2022-09-28 17:35:34 -0400

    save data code

---
+++ Assets/MunroHoberman/SaveData/DataFile.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DataFile
+{
+    public string name;
+    public int health;
+
+    public DataFile(string aName, int aHealth)
+    {
+        name = aName;
+        health = aHealth;
+    }
+}
--- Assets/MunroHoberman/SaveData/SaveCode.cs
+++ Assets/MunroHoberman/SaveData/SaveCode.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -12,12 +13,38 @@ public class SaveCode : MonoBehaviour
 
     public Slider healthSlider;
     
+    public TMP_InputField classJob;
+    
     public void saveData()
     {
-        
+        DataFile data = new DataFile(nameField.text,(int)healthSlider.value);
+        string json=JsonUtility.ToJson(data);
+        System.IO.File.WriteAllText(Application.persistentDataPath+"/save.json",json);
+        Debug.Log(Application.persistentDataPath);
     }
     
-    public void loadData() {
+    public void loadData()
+    {
+        string json=System.IO.File.ReadAllText(Application.persistentDataPath+"/save.json");
+        DataFile data=JsonUtility.FromJson<DataFile>(json);
+        nameField.text=data.name;
+        healthSlider.value = data.health;
+    }
 
+    public void Start()
+    {
+        if (System.IO.File.Exists(Application.persistentDataPath + "/save.json"))
+        {
+            loadData();
+        }
+        else
+        {
+            saveData();
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        saveData();
     }
 }
