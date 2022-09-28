89a4d6f5d197e4fcb2c0847ed9d52aab100c9204
Munro Hoberman <31392444+redmoe@users.noreply.github.com>
2022-09-27 21:25:49 -0400

    setup for class

---
+++ Assets/MunroHoberman/SaveData/SaveCode.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class SaveCode : MonoBehaviour
+{
+
+    public TMP_InputField nameField;
+
+    public Slider healthSlider;
+    
+    public void saveData()
+    {
+        
+    }
+    
+    public void loadData() {
+
+    }
+}
