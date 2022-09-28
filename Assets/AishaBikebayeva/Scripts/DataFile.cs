22743368494efa5cf7b180508b3937e0d3a56d1f
gamesbyaisha <nekoh150@newschool.edu>
2022-09-28 17:12:12 -0400

    Added name input field and scripts for it

---
+++ Assets/AishaBikebayeva/Scripts/DataFile.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AishaGrowth
+{
+    public class DataFile
+    {
+        public string name;
+        public int health;
+
+        public DataFile(string aName, int aHealth)
+        {
+            name = aName;
+            health = aHealth;
+        }
+    }
+}
