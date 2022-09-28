d323aec3ad6eceeccacfecdb1ad0707099a8f501
Regan <102753073+ohh550@users.noreply.github.com>
2022-09-28 13:02:16 -0400

    Change Errors

--- Assets/Regan/Project Growth/Scripts/creature/NetTime.cs
+++ Assets/Regan/Project Growth/Scripts/creature/NetTime.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 using UnityEngine.Networking;
 using System;
 
-namespace Regan {
-
-public class NetTime : MonoBehaviour
+namespace Regan
 {
-    public void getInternetTime(Action<string> onReceiveTime)
-    {
-        StartCoroutine(RequestInternetTime(onReceiveTime));
-    }
 
-    private IEnumerator RequestInternetTime(Action<string> onReceiveTime)
-    {
-        UnityWebRequest myHttpWebRequest = UnityWebRequest.Get("http://www.microsoft.com");
-        yield return myHttpWebRequest.SendWebRequest();
 
-        string netTime = myHttpWebRequest.GetResponseHeader("date");
-        onReceiveTime(netTime);
+    public class NetTime : MonoBehaviour
+    {
+        public void getInternetTime(Action<string> onReceiveTime)
+        {
+            StartCoroutine(RequestInternetTime(onReceiveTime));
+        }
+
+        private IEnumerator RequestInternetTime(Action<string> onReceiveTime)
+        {
+            UnityWebRequest myHttpWebRequest = UnityWebRequest.Get("http://www.microsoft.com");
+            yield return myHttpWebRequest.SendWebRequest();
+
+            string netTime = myHttpWebRequest.GetResponseHeader("date");
+            onReceiveTime(netTime);
+        }
     }
-}
 }
