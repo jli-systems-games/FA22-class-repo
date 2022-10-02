197c4028bb35dcf3166d3f441344361ec9aad31a
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-10-02 17:44:22 -0400

    Trying to change animation

--- Assets/Ekaterina/Growth/Scripts/PetUIController.cs
+++ Assets/Ekaterina/Growth/Scripts/PetUIController.cs
@@ -9,23 +9,32 @@ namespace Ekaterina {
     public class PetUIController : MonoBehaviour
     { 
         public Slider needsSlider;
-        public int petFood = 1;
-        public float needBar = 1;
+        public int petPoint = 1;
+        public float needsBar = 1;
         public static PetUIController instance;
+        
+        private Animator animator;
+        
         private void Start()
         {
+            animator = gameObject.GetComponent<Animator>();
+
             needsSlider.value = 100;
         }
         public void Update()
     {
-        needsSlider.value -= .001f;
+        
+        animator.SetFloat("NeedsValue", needsSlider.value);
+
+        needsSlider.value -= .01f;
+
     }
 
     public void ChangeSomething()
     {
-        if (petFood > 0)
+        if (petPoint > 0)
         {
-            needsSlider.value += needBar;
+            needsSlider.value += needsBar;
         }
     }
     }
