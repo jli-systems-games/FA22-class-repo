80e78775e0cff66037f73dc8993d62b4fbc5dd7f
gamesbyaisha <nekoh150@newschool.edu>
2022-10-03 00:47:56 -0400

    The time sliders work, UI is done, new sprites were added

--- Assets/AishaBikebayeva/Scripts/NeedsControl.cs
+++ Assets/AishaBikebayeva/Scripts/NeedsControl.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
 
 namespace AishaGrowth
 {
@@ -8,6 +10,26 @@ namespace AishaGrowth
     {
         public int food, happiness, energy;
         public int foodTickRate, happinessTickRate, energyTickRate;
+        public DateTime lastTimeFed, lastTimeGainedEnergy, lastTimeHappy;
+
+        private void Awake()
+        {
+            Initialize(100,100,100,5,7,15);
+        }
+
+        public void Initialize(int food, int happiness, int energy, int foodTickRate, int happinessTickRate, int energyTickRate)
+        {
+            lastTimeFed = DateTime.Now;
+            lastTimeHappy = DateTime.Now;
+            lastTimeGainedEnergy = DateTime.Now;
+            this.food=food;
+            this.happiness=happiness;
+            this.energy=energy;
+            this.foodTickRate=foodTickRate;
+            this.happinessTickRate=happinessTickRate;
+            this.energyTickRate=energyTickRate;
+            AishaGrowth.PetUIController.instance.UpdateImages(food,happiness,energy);
+        }
 
         public void Initialize(int food, int happiness, int energy)
         {
@@ -17,6 +39,7 @@ namespace AishaGrowth
             this.foodTickRate=foodTickRate;
             this.happinessTickRate=happinessTickRate;
             this.energyTickRate=energyTickRate;
+            AishaGrowth.PetUIController.instance.UpdateImages(food,happiness,energy);
         }
         // Start is called before the first frame update
         void Start()
@@ -32,6 +55,7 @@ namespace AishaGrowth
                 ChangeFood(-foodTickRate);
                 ChangeEnergy(-energyTickRate);
                 ChangeHappiness(-happinessTickRate);
+                AishaGrowth.PetUIController.instance.UpdateImages(food,happiness,energy);
             }
         }
 
@@ -41,6 +65,7 @@ namespace AishaGrowth
             if(food < 0)
             {
                 PetManager.Die();
+                SceneManager.LoadSceneAsync("GameOver");
             }
             else if (food > 100) food = 100;
         }
@@ -50,6 +75,7 @@ namespace AishaGrowth
             if(happiness < 0)
             {
                 PetManager.Die();
+                SceneManager.LoadSceneAsync("GameOver");
             }
             else if (happiness > 100) happiness = 100;
         }
@@ -59,6 +85,7 @@ namespace AishaGrowth
             if(energy < 0)
             {
                 PetManager.Die();
+                SceneManager.LoadSceneAsync("GameOver");
             }
             else if (energy > 100) energy = 100;
         }
