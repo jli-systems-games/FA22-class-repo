945743ea25b40eb04dfea776c85342e107b4c7e2
Weeeeeeeeei <wuw519@newschool.edu>
2022-10-03 19:23:49 -0400

    Start of the stage 2

--- Assets/Wei Grow/WGScripts/ClickEffect.cs
+++ Assets/Wei Grow/WGScripts/ClickEffect.cs
@@ -2,26 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickEffect : MonoBehaviour
+
+
+namespace Bloom
 {
-    private Vector3 point;
-    public GameObject effectGo;
-
-    void Start()
+    public class ClickEffect : MonoBehaviour
     {
-       
-    }
+        private Vector3 point;
+        public GameObject effectGo;
 
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+        void Start()
         {
-            point = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 4f);//获得鼠标点击点
-            point = Camera.main.ScreenToWorldPoint(point);//从屏幕空间转换到世界空间
-            GameObject go = Instantiate(effectGo);//生成特效
-            go.transform.position = point;
-            Destroy(go, 0.5f);
+
+        }
+
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                point = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 4f);
+                point = Camera.main.ScreenToWorldPoint(point);
+                GameObject go = Instantiate(effectGo);
+                go.transform.position = point;
+                Destroy(go, 0.5f);
+            }
         }
     }
 }
