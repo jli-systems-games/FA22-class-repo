d7ae1a011dd35ca3a5c29111049fa375839a4621
gamesbyaisha <nekoh150@newschool.edu>
2022-10-03 00:52:20 -0400

    Added timer

---
+++ Assets/AishaBikebayeva/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace AishaGrowth
+{
+    public class Timer : MonoBehaviour
+    {
+        public TMP_Text textdisplay;
+        public Image bardisplay;
+
+        public float timerDuration = 30;
+        float timer = 0f;
+        // Start is called before the first frame update
+        void Start()
+        {
+            timer = timerDuration;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            timer -= Time.deltaTime;
+            textdisplay.text = "Time left: " + timer.ToString("0") + " secs";
+            // bardisplay.fillAmount = timer / timerDuration;
+
+            if (timer < 0)
+            {
+                timer = timerDuration;
+                Debug.Log("Time has ended!");
+                SceneManager.LoadScene("Win");
+            }
+
+        }
+    }
+}
