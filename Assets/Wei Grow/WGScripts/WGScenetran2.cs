78b4beb0c15158601cd2d302c26211fb74160565
Weeeeeeeeei <wuw519@newschool.edu>
2022-10-04 05:36:45 -0400

    DOWN

---
+++ Assets/Wei Grow/WGScripts/WGScenetran2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WGScenetran2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SceneTran2()
+    {
+        SceneManager.LoadScene("WG_StartScene");
+    }
+}
