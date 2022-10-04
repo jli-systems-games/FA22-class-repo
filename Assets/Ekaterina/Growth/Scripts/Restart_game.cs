721946ec62a4b65c62886440b06ea48c9fde3ff6
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-10-03 23:04:13 -0400

    Final changes

---
+++ Assets/Ekaterina/Growth/Scripts/Restart_game.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Ekaterina
+{
+    public class Restart_game : MonoBehaviour
+    {
+        public Button Button;
+        void Start () {
+            Button btn = Button.GetComponent<Button>();
+            btn.onClick.AddListener(TaskOnClick);
+        }
+        void TaskOnClick(){
+            SceneManager.LoadScene("Start");
+        }
+    }
+}
