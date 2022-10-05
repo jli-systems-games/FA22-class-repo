9f5d69aa2b0ec0985f19cc1651ba47b5d32ea6c2
Regan <102753073+ohh550@users.noreply.github.com>
2022-10-05 13:02:50 -0400

    Finals

---
+++ Assets/Regan/ProjectGrowthFinal/Scripts/creature/GrowthStages/TeenStage.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReganFinal
+{
+    [CreateAssetMenu(menuName = "ReganFinal/GrowthStages/TeenStage")]
+    public class TeenStage : GrowthStageBase
+    {
+        public override void UpdateAge()
+        {
+            base.UpdateAge();
+        }
+    }
+}
