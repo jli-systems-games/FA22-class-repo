08784520dd56c456f7773018f0d1dccd44f19db5
DinosaurPower <lysym685@newschool.edu>
2022-10-07 16:12:55 -0400

    Sin of making equipment system
    FInally made nice limb switch, soon I'll move to some simple UI and one more enemy

---
+++ Assets/MaiTrueRogue/Scripts/RogueLimb.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Bananagodzilla
+{
+
+    public class RogueLimb : MonoBehaviour
+    {
+        public int rage;
+
+        public int sugar;
+
+        public int bones;
+
+        public string limbType;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
