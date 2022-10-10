f4b65bb26455057faa68e924bd2f5c19fb18ecf3
zaksaja <81482722+zaksaja@users.noreply.github.com>
2022-10-10 17:15:50 -0400

    First commit

---
+++ Assets/samantha zak/Arcade Game/Scripts/StarCollect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarCollect : MonoBehaviour
+{
+   
+    private int star = 0;
+    
+   
+    [SerializeField] private Text starText;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Star"))
+        {
+            Destroy(collision.gameObject);
+            star++;
+            starText.text = "Stars: " + star;
+            
+            
+        }
+        
+    }
+    
+}
