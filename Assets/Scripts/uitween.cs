f0531448db8e03621d1491230cbdbe7b609fe737
gamesbyaisha <nekoh150@newschool.edu>
2022-10-10 09:35:54 -0400

    Added Leantween package

---
+++ Assets/Scripts/uitween.cs
@@ -0,0 +1,21 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class uitween : MonoBehaviour
+// {
+//     [SerializeField]
+
+//     GameObject background;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         LeanTween.scale(background, new Vector3(1.1f, 1.1f, 1.1f), 2f).setDelay(.5f).setEase(LeanTweenType.easeOutElastic);
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
