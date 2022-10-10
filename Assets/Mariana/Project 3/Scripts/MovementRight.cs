d11220d5c6d945a271bed45e450efe93b071c8b4
m-holtz <74580132+m-holtz@users.noreply.github.com>
2022-10-10 00:57:53 -0400

    Leg Movement

---
+++ Assets/Mariana/Project 3/Scripts/MovementRight.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mariana
+{
+    public class MovementRight : MonoBehaviour
+    {
+        private Rigidbody2D leftLeg;
+
+        //[SerializeField] private float speed = 18f;
+        //[SerializeField] private HingeJoint2D leftHinge;
+        //[SerializeField] private HingeJoint2D rightHinge;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            leftLeg = transform.GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.O))
+            {
+                leftLeg.MovePosition(transform.position + Vector3.left);
+            }
+            if (Input.GetKey(KeyCode.P))
+            {
+                leftLeg.MovePosition(transform.position + Vector3.right);
+            }
+            // if (Input.GetKeyDown(KeyCode.Space))
+            // {
+
+            // rightHinge.useMotor = Input.GetAxis("Horizontal") > 0;
+            // leftHinge.useMotor = true;
+            // }
+            // else
+            // {
+            //    leftHinge.useMotor = false;
+            //}
+        }
+    }
+
+}
