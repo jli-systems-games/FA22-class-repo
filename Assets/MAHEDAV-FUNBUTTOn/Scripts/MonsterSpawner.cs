15e1f98dbc8b95ea59a5bccda6a4347984276fd5
Hectorgamecreator <rodrh308@newschool.edu>
2022-10-11 10:56:53 -0400

    added namespace
    added namespace

--- Assets/MAHEDAV-FUNBUTTOn/Scripts/MonsterSpawner.cs
+++ Assets/MAHEDAV-FUNBUTTOn/Scripts/MonsterSpawner.cs
@@ -2,36 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterSpawner : MonoBehaviour
+namespace HectorRodriguez
 {
-    public Monster monsterPrefab;
-    public float spawnDistance = 12f;
-    public float spawnRate = 1f;
-    public int amountPerSpawn = 1;
-    [Range(0f, 45f)] public float trajectoryVariance = 15f;
 
-    private void Start()
+    public class MonsterSpawner : MonoBehaviour
     {
-        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
-    }
+        public Monster monsterPrefab;
+        public float spawnDistance = 12f;
+        public float spawnRate = 1f;
+        public int amountPerSpawn = 1;
+        [Range(0f, 45f)] public float trajectoryVariance = 15f;
 
-    public void Spawn()
-    {
-        for (int i = 0; i < amountPerSpawn; i++)
+        private void Start()
         {
-            Vector2 spawnDirection = Random.insideUnitCircle.normalized;
-            Vector3 spawnPoint = spawnDirection * spawnDistance;
+            InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+        }
 
-            spawnPoint += transform.position;
+        public void Spawn()
+        {
+            for (int i = 0; i < amountPerSpawn; i++)
+            {
+                Vector2 spawnDirection = Random.insideUnitCircle.normalized;
+                Vector3 spawnPoint = spawnDirection * spawnDistance;
 
-            float variance = Random.Range(-trajectoryVariance, trajectoryVariance);
-            Quaternion rotation = Quaternion.AngleAxis(variance, Vector3.forward);
+                spawnPoint += transform.position;
 
-            Monster asteroid = Instantiate(monsterPrefab, spawnPoint, rotation);
-           
+                float variance = Random.Range(-trajectoryVariance, trajectoryVariance);
+                Quaternion rotation = Quaternion.AngleAxis(variance, Vector3.forward);
 
-            Vector2 trajectory = rotation * -spawnDirection;
-            asteroid.SetTrajectory(trajectory);
+                Monster monster = Instantiate(monsterPrefab, spawnPoint, rotation);
+
+
+                Vector2 trajectory = rotation * -spawnDirection;
+                monster.SetTrajectory(trajectory);
+            }
         }
     }
 }
