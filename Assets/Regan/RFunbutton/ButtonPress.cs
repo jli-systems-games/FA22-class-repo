55f5d0cf8f266da6904b4f779e90f9987bc54ea4
MarioBrnjac <90148758+MarioBrnjac@users.noreply.github.com>
2022-10-11 10:10:39 -0400

    Button spawns balls

--- Assets/Regan/RFunbutton/ButtonPress.cs
+++ Assets/Regan/RFunbutton/ButtonPress.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonPress : MonoBehaviour
+namespace ReganFunButton
 {
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(pos, Vector3.zero);
 
-            if (hit && hit.collider != null)
+    public class ButtonPress : MonoBehaviour
+    {
+        public GameObject Runt;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
             {
-                Debug.Log("Object Name:" + hit.collider.gameObject.name);
+                Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(pos, Vector3.zero);
+                Vector3 spawn = new Vector3(Random.Range(-1f, 2f), Random.Range(-1f, 1f), 0f);
+
+                if (hit && hit.collider != null)
+                {
+                    Debug.Log("Object Name:" + hit.collider.gameObject.name);
+                    Instantiate(Runt, spawn, Quaternion.identity);
+                }
             }
         }
     }
