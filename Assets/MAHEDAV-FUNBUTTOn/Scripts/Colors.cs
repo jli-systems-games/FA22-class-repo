95fc6c0bb5d2b5a1d9d1ed6355f5782f75e01d37
DinosaurPower <lysym685@newschool.edu>
2022-10-11 10:19:06 -0400

    COlor changing playtest

--- Assets/MAHEDAV-FUNBUTTOn/Scripts/Colors.cs
+++ Assets/MAHEDAV-FUNBUTTOn/Scripts/Colors.cs
@@ -1,18 +1,34 @@
 
 using UnityEngine;
 using UnityEngine.UI;
-namespace mahedav
-{
-    public Image monsterSprite;
+namespace mahedav {
+    
+    
+    
     public class Colors : MonoBehaviour
     {
+
+        public Image monsterColor;
+        
+       
         // Start is called before the first frame update
         void Start()
         {
-            monsterSprite = GameObject.GetComponent<Image>().color =
+            monsterColor = gameObject.GetComponent<Image>();
+            
+         monsterColor.color =
                 new Color(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
-Debug.Log(color);
+Debug.Log(monsterColor.color);
         }
+        
+        
+        void Update()
+        {
+            gameObject.GetComponent<Image>().color =
+                new Color(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
+            Debug.Log(monsterColor.color);
+        }
+        
 
         
     }
