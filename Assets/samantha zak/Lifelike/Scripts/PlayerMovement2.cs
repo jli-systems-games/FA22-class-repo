1a71e1cada5dd163bd3a23258667682fd860ce90
zaksaja <81482722+zaksaja@users.noreply.github.com>
2022-10-12 17:29:58 -0400

    namespace

--- Assets/samantha zak/Lifelike/Scripts/PlayerMovement2.cs
+++ Assets/samantha zak/Lifelike/Scripts/PlayerMovement2.cs
@@ -2,32 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class PlayerMovement : MonoBehaviour
+namespace Sam
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
-
-    Vector2 movement;
-    void Update()
+    public class PlayerMovement : MonoBehaviour
     {
-        //Input will go here
+        public float moveSpeed = 5f;
+        public Rigidbody2D rb;
 
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        
+        Vector2 movement;
+
+        void Update()
+        {
+            //Input will go here
+
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+
+        }
+
+        void FixedUpdate()
+        {
+            // The movement goes here
+
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        }
+
+
+
     }
-
-    void FixedUpdate()
-    {
-        // The movement goes here
-
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-    }
-    
-    
-
 }
 
 
