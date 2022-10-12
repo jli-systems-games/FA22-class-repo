c2d2312a73644bd87801a5c16f5e61017f3bad23
Munro Hoberman <31392444+redmoe@users.noreply.github.com>
2022-10-12 14:08:28 -0400

    code

---
+++ Assets/MunroHoberman/StateMachines/StateMachineGuard/GuardBaseState.cs
@@ -0,0 +1,10 @@
+using UnityEngine.AI;
+namespace MunroHoberman.StateMachine
+{
+    public class GuardBaseState
+    {
+        public virtual void EnterState(GuardStateManager guard){}
+
+        public virtual void UpdateState(GuardStateManager guard){}
+    }
+}
---
+++ Assets/MunroHoberman/StateMachines/StateMachineGuard/GuardStateManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace MunroHoberman.StateMachine {
+    public class GuardStateManager : MonoBehaviour
+    {
+        
+        public int _targetIndex = 0;
+        public Transform _player;
+        public Transform[] waypoints;
+        public UnityEngine.AI.NavMeshAgent _agent;
+        public float sight = 10f;
+
+
+        public GuardBaseState currentState;
+        public GuardPatrolState patrolState= new GuardPatrolState();
+        public GuardChaseState chaseState= new GuardChaseState();
+
+        void Start()
+        {
+            _agent = transform.GetComponent<UnityEngine.AI.NavMeshAgent>();
+            SwitchState(patrolState);
+        }
+
+        void Update()
+        {
+            currentState.UpdateState(this);
+        }
+
+        public void SwitchState(GuardBaseState state)
+        {
+            currentState = state;
+            state.EnterState(this);
+        }
+    }
+}
