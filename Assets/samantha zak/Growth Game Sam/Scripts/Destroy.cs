7f625451a021140dc5d50008775b726e7db92569
zaksaja <81482722+zaksaja@users.noreply.github.com>
2022-10-12 17:35:43 -0400

    fix

--- Assets/samantha zak/Growth Game Sam/Scripts/Destroy.cs
+++ Assets/samantha zak/Growth Game Sam/Scripts/Destroy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
+namespace Sam
+{
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Leaf"))
@@ -16,4 +18,5 @@ public class Destroy : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
+}
 }
