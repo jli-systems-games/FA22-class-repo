7a286a114d2940d48d532dfd447847e2baa23a4c
David <duy312@newschool.edu>
2022-10-14 01:48:49 -0400

    update

--- Assets/DavidProjects/Project3/Script/BuildingManager.cs
+++ Assets/DavidProjects/Project3/Script/BuildingManager.cs
@@ -1,33 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BuildingManager : MonoBehaviour
 {
 
+    public static BuildingManager Instance { get; private set; }
+
+
+
     private Camera mainCamera;
     private BuildingTypeListSO buildingTypeList;
-    private BuildingTypeSO buildingType;
-    
-    
-    private void Start() {
-        mainCamera = Camera.main;
+    private BuildingTypeSO activeBuildingType;
+
+    private void Awake() {
+        Instance = this;
 
         buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
-        buildingType = buildingTypeList.list[0];
     }
 
-    private void Update(){
-        if (Input.GetMouseButtonDown(0)) {
-            Instantiate(buildingType.prefab,GetMouseWorldPosition(),Quaternion.identity);
-        }
+    private void Start() {
+        mainCamera = Camera.main;
+    }
 
-        if (Input.GetKeyDown(KeyCode.T)) {
-            buildingType = buildingTypeList.list[0];
-        }
-
-        if (Input.GetKeyDown(KeyCode.Y)) {
-            buildingType = buildingTypeList.list[1];
+    private void Update() {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
+            if (activeBuildingType != null) {
+                Instantiate(activeBuildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+            }
         }
     }
 
@@ -36,4 +37,13 @@ public class BuildingManager : MonoBehaviour
         mouseWorldPosition.z = 0f;
         return mouseWorldPosition;
     }
+
+    public void SetActiveBuildingType(BuildingTypeSO buildingType) {
+        activeBuildingType = buildingType;
+    }
+
+    public BuildingTypeSO GetActiveBuildingType() {
+        return activeBuildingType;
+    }
+
 }
