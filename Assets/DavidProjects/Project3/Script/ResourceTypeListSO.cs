193d24b924facb6930a58adf0690cdd9e65df2f7
David <duy312@newschool.edu>
2022-10-14 22:33:20 -0400

    Update

--- Assets/DavidProjects/Project3/Script/ResourceTypeListSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ScriptableObjects/ResourceTypeList")]
-public class ResourceTypeListSO : ScriptableObject
-{
-   public List<ResourceTypeSO> list;
-}
