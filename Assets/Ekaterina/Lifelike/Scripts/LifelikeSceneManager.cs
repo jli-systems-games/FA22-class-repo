87fb34ce0550b388dc23f27ea76b0f3ef219006d
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-10-16 17:03:04 -0400

    Collision added

---
+++ Assets/Ekaterina/Lifelike/Scripts/LifelikeSceneManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Ekaterina
+{
+    public class LifelikeSceneManager : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider.gameObject.name == "Alien 1")
+            {
+                SceneManager.LoadScene("Ekaterina.Lifelike.Start");
+            }
+        }
+    }
+}
+    
+
+
