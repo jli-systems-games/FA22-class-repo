88fedd191cf70781d684fdaf6f3816902e0005e0
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-10-17 17:26:22 -0400

    The MarsPlayer script implemented

--- Assets/Ekaterina/Lifelike/Scripts/Guard.cs
+++ Assets/Ekaterina/Lifelike/Scripts/Guard.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -13,37 +14,69 @@ namespace Ekaterina {
         public Transform playerTarget;
         private int _targetIndex = 0;
         public float sight = 10f;
+
+
+        public Transform lastActiveWayPoint; //to keep track of where we were going so we know to go back to it
+        public Transform activeTargetLocation;// for debugging
         
+        private bool _isChasing = false;
+        int layerMask = 1 << 3;
+
         // Start is called before the first frame update
         void Start()
         {
             _agent = transform.GetComponent<NavMeshAgent>();
+            activeTargetLocation = lastActiveWayPoint = targetLocations[0]; //set the default targets
         }
-    
+
         // Update is called once per frame
         void Update()
         {
-            int layerMask = 1 << 3;
-            if (Vector3.Distance(transform.position,playerTarget.position)<sight 
-                && !Physics.Linecast (transform.position, playerTarget.transform.position,layerMask)) {
-                _agent.SetDestination(playerTarget.position);
+            //if the player is suddenly shaded
+            if (playerTarget.GetComponent<MarsPlayer>().IsShaded)
+            {
+                _agent.SetDestination(lastActiveWayPoint.position);
+                activeTargetLocation = lastActiveWayPoint;
             }
-            if (!_agent.pathPending)
-            { 
-                if (_agent.remainingDistance <= _agent.stoppingDistance)
+            //if the player is still visible
+            if (IsPlayerVisible())
+            {
+                lastActiveWayPoint = targetLocations[_targetIndex];
+                _agent.SetDestination(playerTarget.position);
+                activeTargetLocation = playerTarget;
+            }
+            else //if we're normally moving, this remains unchanged
+            {
+                if (!_agent.pathPending)
                 {
-                    if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
+                    if (_agent.remainingDistance <= _agent.stoppingDistance)
                     {
-                        GoToNextLocation();
+                        if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
+                        {
+                            GoToNextLocation();
+                        }
                     }
                 }
             }
+            
+        }
+
+
+        // Separate function because we will do this evaluation mutiple times
+        bool IsPlayerVisible()
+        {
+            return Vector3.Distance(transform.position, playerTarget.position) < sight
+                && !Physics.Linecast(transform.position, playerTarget.transform.position, layerMask) 
+                && !playerTarget.GetComponent<MarsPlayer>().IsShaded;
+            
         }
 
         public void GoToNextLocation()
         {
             _agent.SetDestination(targetLocations[_targetIndex].position);
             _targetIndex = (_targetIndex+ 1) % targetLocations.Length;
+            
+            activeTargetLocation = targetLocations[_targetIndex];
         }
         
     }
---
+++ Assets/Ekaterina/Lifelike/Scripts/MarsPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Ekaterina
+{
+    public class MarsPlayer : MonoBehaviour
+    {
+        public bool IsShaded { get; private set; } = false;
+
+        void Update()
+        {
+            // TODO make this use OnTriggerEnter/Exit instead of Update
+            if (Input.GetKey(KeyCode.Space))
+            {
+                IsShaded = true;
+            }
+            else
+            {
+                IsShaded = false;
+            }
+        }
+    }
+}
