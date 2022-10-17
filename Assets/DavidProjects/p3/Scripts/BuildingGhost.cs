68b5c12779331960e649dc560fb1b59b08fc8b3b
David <duy312@newschool.edu>
2022-10-17 17:37:21 -0400

    Bring over existing project 3 assets

---
+++ Assets/DavidProjects/p3/Scripts/BuildingGhost.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingGhost : MonoBehaviour {
+
+    private GameObject spriteGameObject;
+
+    private void Awake() {
+        spriteGameObject = transform.Find("sprite").gameObject;
+
+        Hide();
+    }
+
+    private void Start() {
+        BuildingManager.Instance.OnActiveBuildingTypeChanged += BuildingManager_OnActiveBuildingTypeChanged;
+    }
+
+    private void BuildingManager_OnActiveBuildingTypeChanged(object sender, BuildingManager.OnActiveBuildingTypeChangedEventArgs e) {
+        if (e.activeBuildingType == null) {
+            Hide();
+        } else {
+            Show(e.activeBuildingType.sprite);
+        }
+    }
+
+    private void Update() {
+        transform.position = UtilsClass.GetMouseWorldPosition();
+    }
+
+    private void Show(Sprite ghostSprite) {
+        spriteGameObject.SetActive(true);
+        spriteGameObject.GetComponent<SpriteRenderer>().sprite = ghostSprite;
+    }
+
+    private void Hide() {
+        spriteGameObject.SetActive(false);
+    }
+
+
+}
---
+++ Assets/DavidProjects/p3/Scripts/ResourceGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceGenerator : MonoBehaviour {
+
+    private ResourceGeneratorData resourceGeneratorData;
+    private float timer;
+    private float timerMax;
+
+    private void Awake() {
+        resourceGeneratorData = GetComponent<BuildingTypeHolder>().buildingType.resourceGeneratorData;
+        timerMax = resourceGeneratorData.timerMax;
+    }
+
+    private void Start() {
+        Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(transform.position, resourceGeneratorData.resourceDetectionRadius);
+
+        int nearbyResourceAmount = 0;
+        foreach (Collider2D collider2D in collider2DArray) {
+            ResourceNode resourceNode = collider2D.GetComponent<ResourceNode>();
+            if (resourceNode != null) {
+                // It's a resource node!
+                if (resourceNode.resourceType == resourceGeneratorData.resourceType) {
+                    // Same type!
+                    nearbyResourceAmount++;
+                }
+            }
+        }
+
+        nearbyResourceAmount = Mathf.Clamp(nearbyResourceAmount, 0, resourceGeneratorData.maxResourceAmount);
+
+        if (nearbyResourceAmount == 0) {
+            // No resource nodes nearby
+            // Disable resource generator
+            enabled = false;
+        } else {
+            timerMax = (resourceGeneratorData.timerMax / 2f) +
+                resourceGeneratorData.timerMax *
+                (1 - (float)nearbyResourceAmount / resourceGeneratorData.maxResourceAmount);
+        }
+
+        Debug.Log("nearbyResourceAmount: " + nearbyResourceAmount + "; timerMax: " + timerMax);
+    }
+
+    private void Update() {
+        timer -= Time.deltaTime;
+        if (timer <= 0f) {
+            timer += timerMax;
+            ResourceManager.Instance.AddResource(resourceGeneratorData.resourceType, 1);
+        }
+    }
+}
