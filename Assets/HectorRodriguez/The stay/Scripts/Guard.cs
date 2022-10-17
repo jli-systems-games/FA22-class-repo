4517e5df4463a0f2e23d52537f9999197bab07a0
Hectorgamecreator <rodrh308@newschool.edu>
2022-10-17 17:49:10 -0400

    change guard script

--- Assets/HectorRodriguez/The stay/Scripts/Guard.cs
+++ Assets/HectorRodriguez/The stay/Scripts/Guard.cs
@@ -6,40 +6,75 @@ using UnityEngine.AI;
 namespace HectorRodriguez.Pathfinding { 
 public class Guard : MonoBehaviour
 {
-    private NavMeshAgent _agent;
-    public Transform[] waypoints;
-    private Transform _player;
-    private int _targetIndex = 0;
-    public float sight = 10f;
+        NavMeshAgent _agent;
+        public Transform[] targetLocations;
+        public Transform playerTarget;
+        private int _targetIndex = 0;
+        public float sight = 10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _agent = transform.GetComponent<NavMeshAgent>();
-        _player = GameObject.FindWithTag("Player").transform;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        public Transform lastActiveWayPoint; //to keep track of where we were going so we know to go back to it
+        public Transform activeTargetLocation;// for debugging
+
+        private bool _isChasing = false;
         int layerMask = 1 << 3;
-        if (Vector3.Distance(transform.position, _player.position) < sight
-            && !Physics.Linecast(transform.position, _player.transform.position, layerMask))
+
+        // Start is called before the first frame update
+        void Start()
         {
-            _agent.SetDestination(_player.position);
-        }
-        if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
-        {
-            GoToNextLocation();
-        }
-            _agent.SetDestination(_player.position);
+            _agent = transform.GetComponent<NavMeshAgent>();
+            activeTargetLocation = lastActiveWayPoint = targetLocations[0]; //set the default targets
         }
 
-    public void GoToNextLocation()
-    {
-        _agent.SetDestination(waypoints[_targetIndex].position);
-        _targetIndex = (_targetIndex + 1) % waypoints.Length;
+        // Update is called once per frame
+        void Update()
+        {
+            //if the player is suddenly shaded
+            if (playerTarget.GetComponent<FPSPlayer>().IsShaded)
+            {
+                _agent.SetDestination(lastActiveWayPoint.position);
+                activeTargetLocation = lastActiveWayPoint;
+            }
+            //if the player is still visible
+            if (IsPlayerVisible())
+            {
+                lastActiveWayPoint = targetLocations[_targetIndex];
+                _agent.SetDestination(playerTarget.position);
+                activeTargetLocation = playerTarget;
+            }
+            else //if we're normally moving, this remains unchanged
+            {
+                if (!_agent.pathPending)
+                {
+                    if (_agent.remainingDistance <= _agent.stoppingDistance)
+                    {
+                        if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
+                        {
+                            GoToNextLocation();
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+        // Separate function because we will do this evaluation mutiple times
+        bool IsPlayerVisible()
+        {
+            return Vector3.Distance(transform.position, playerTarget.position) < sight
+                && !Physics.Linecast(transform.position, playerTarget.transform.position, layerMask)
+                && !playerTarget.GetComponent<MarsPlayer>().IsShaded;
+
+        }
+
+        public void GoToNextLocation()
+        {
+            _agent.SetDestination(targetLocations[_targetIndex].position);
+            _targetIndex = (_targetIndex + 1) % targetLocations.Length;
+
+            activeTargetLocation = targetLocations[_targetIndex];
+        }
+
     }
-
-}
 }
