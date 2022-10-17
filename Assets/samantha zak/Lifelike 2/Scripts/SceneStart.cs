d7905c225ff7872f4b371516dd66a67b71fa1a06
zaksaja <81482722+zaksaja@users.noreply.github.com>
2022-10-17 18:18:56 -0400

    FINALFINAL because i had the wrong folder

---
+++ Assets/samantha zak/Lifelike 2/Scripts/SceneStart.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneStart : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    /*public void Controls()
+    {
+        SceneManager.LoadScene("Controls Page");
+    }
+    */
+
+
+}
