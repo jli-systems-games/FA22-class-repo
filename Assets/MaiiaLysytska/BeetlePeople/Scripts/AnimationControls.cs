ab8633de778781c47edc220846dccc82be8857e5
DinosaurPower <lysym685@newschool.edu>
2022-10-21 00:02:28 -0400

    sin of making basic mechanics and not commiting for two hours
    Added sprites, animations, codes and prefabs

---
+++ Assets/MaiiaLysytska/BeetlePeople/Scripts/AnimationControls.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bananagodzilla
+{
+
+public class AnimationControls : MonoBehaviour
+{
+    public Animator anim;
+
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponentInParent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.y > 1 || rb.velocity.y < -1 || rb.velocity.x < -1 || rb.velocity.x > 1)
+        {
+            anim.SetBool("IsWalking", true);
+        }
+        else{ anim.SetBool("IsWalking", false);}
+
+        if (rb.velocity.x < -1)
+        {
+            gameObject.GetComponent<Transform>().localScale = new Vector3(-1, 1, 0);
+        }
+        
+        if (rb.velocity.x > 1)
+        {
+            gameObject.GetComponent<Transform>().localScale = new Vector3(1, 1, 0);
+        }
+    }
+}
+    
+}
---
+++ Assets/MaiiaLysytska/BeetlePeople/Scripts/BeetleLoose.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Ekaterina;
+using UnityEngine;
+
+namespace Bananagodzilla
+{
+    
+
+public class BeetleLoose : MonoBehaviour
+{
+
+    public GameObject leaf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject == leaf)
+        {
+            Lose();
+        }
+    }
+
+    void Lose()
+    {
+        Debug.Log("Lost");
+    }
+}
+}
