6055b989bb2b85122c92326f2e6b22607316259a
Jason Li <jasonli1337@gmail.com>
2022-10-21 14:50:24 -0400

    Cherry pick animation

---
+++ Assets/MaiTrueRogue/Scripts/RogueExplosion.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace  Bananagodzilla{
+    public class RogueExplosion: MonoBehaviour
+    {
+
+
+
+        public float fl;
+        public GameObject[] limbs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fl = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Explode()
+    {
+        foreach (var limb in limbs)
+        {
+            limb.GetComponent<Rigidbody2D>().velocity =
+                new Vector2(Random.Range(-fl, fl), Random.Range(-fl, fl));
+        }
+    }
+}
+}
