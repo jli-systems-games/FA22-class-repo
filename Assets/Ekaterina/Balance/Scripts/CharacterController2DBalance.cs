cda6d2973649e6091a401ac830d8cc8107574d42
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-10-22 18:37:24 -0400

    Trail added

--- Assets/Ekaterina/Balance/Scripts/CharacterController2DBalance.cs
+++ Assets/Ekaterina/Balance/Scripts/CharacterController2DBalance.cs
@@ -86,9 +86,8 @@ public class CharacterController2DBalance : MonoBehaviour
 		// If the player should jump...
 		if (m_Grounded && jump)
 		{
-					    CreateParticles();
+		    CreateParticles();
 
-		
 			// Add a vertical force to the player.
 			m_Grounded = false;
 			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
