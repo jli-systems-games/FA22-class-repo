05f195322c135c2f46656ac9a4291f6b2790efe6
David <duy312@newschool.edu>
2022-10-21 21:11:42 -0700

    Final

---
+++ Assets/DavidProjects/DavidLifelike/Scripts/GameSceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DavidLifelike {
+public static class GameSceneManager {
+
+    public enum Scene {
+        GameScene,
+        MainMenuScene,
+    }
+
+    public static void Load(Scene scene) {
+        SceneManager.LoadScene(scene.ToString());
+    }
+ }
+}
