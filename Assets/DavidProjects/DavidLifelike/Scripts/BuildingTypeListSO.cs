cdf41c1bd77480a8fd902d367a4c3da4a1c3ab66
David <duy312@newschool.edu>
2022-10-21 21:11:42 -0700

    Final

---
+++ Assets/DavidProjects/DavidLifelike/Scripts/BuildingTypeListSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DavidLifelike {
+[CreateAssetMenu(menuName = "ScriptableObjects/BuildingTypeList")]
+public class BuildingTypeListSO : ScriptableObject {
+
+    public List<BuildingTypeSO> list;
+ }
+}
---
+++ Assets/DavidProjects/DavidLifelike/Scripts/ResourceTypeSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DavidLifelike {
+[CreateAssetMenu(menuName = "ScriptableObjects/ResourceType")]
+public class ResourceTypeSO : ScriptableObject {
+
+    public string nameString;
+    public string nameShort;
+    public Sprite sprite;
+    public string colorHex;
+
+    }
+}
