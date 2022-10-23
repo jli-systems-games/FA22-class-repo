0ac740458bfa6211b58a987fa692de72a73cd390
Jason Li <jasonli1337@gmail.com>
2022-10-23 01:27:37 -0400

    Move Aisha's TextFX to Shared Assets
    And update Assemblies accordingly

---
+++ Assets/Shared Assets/TextFx/Editor/MenuOptionsTextFxNative.cs
@@ -0,0 +1,22 @@
+﻿using Shared_Assets.TextFx.Scripts;
+using UnityEditor;
+using UnityEngine;
+
+namespace Shared_Assets.TextFx.Editor
+{
+	public static class MenuOptionsTextFxNative
+	{
+		public const string NEW_INSTANCE_NAME = "TextFx Text";
+
+		[MenuItem("GameObject/TextFx/Text", false)]
+		static public void AddTextFxNativeInstance ()
+		{
+			GameObject go = new GameObject (NEW_INSTANCE_NAME);
+			
+			TextFxNative textfxComp = go.AddComponent<TextFxNative>();
+			textfxComp.SetText("New TextFx");
+			
+			Selection.activeGameObject = go;
+		}
+	}
+}
