7875bba98bfce76382fb204e78987aea61d5966e
zaksaja <81482722+zaksaja@users.noreply.github.com>
2022-10-24 18:27:36 -0400

    Player move

---
+++ Assets/samantha zak/Balance/Scripts/MovePlayer2D.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer2D : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    [SerializeField] float moveSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
