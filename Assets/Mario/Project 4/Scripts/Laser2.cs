702d19b6f3b0017e85bee706660a891301b3f5b3
MarioBrnjac <90148758+MarioBrnjac@users.noreply.github.com>
2022-10-24 18:38:56 -0400

    Added sprites and cooldowns

--- Assets/Mario/Project 4/Scripts/Laser2.cs
+++ Assets/Mario/Project 4/Scripts/Laser2.cs
@@ -10,19 +10,25 @@ namespace MarioBalance
         public Camera cam;
         public SpriteRenderer sprite;
         public Transform firePoint;
+        public BoxCollider2D boxCollider;
+
+        private bool canShoot;
 
         // Start is called before the first frame update
         void Start()
         {
+            canShoot = true;
             DisableLaser();
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetButtonDown("Fire2"))
+            if (Input.GetButtonDown("Fire2") && canShoot == true)
             {
                 EnableLaser();
+                canShoot = false;
+                StartCoroutine(ShootCooldown(2f));
             }
 
             if (Input.GetButton("Fire2"))
@@ -36,8 +42,20 @@ namespace MarioBalance
             }
         }
 
+        private IEnumerator ShootCooldown(float cooldown)
+        {
+            //Cooldown
+            // Everything Here Happens BEFORE Our Wait
+            canShoot = false;
+
+            yield return new WaitForSeconds(cooldown);
+            //Can Shoot Again
+            canShoot = true;
+        }
+
         void EnableLaser()
         {
+            boxCollider.enabled = true;
             sprite.enabled = true;
         }
 
@@ -48,6 +66,7 @@ namespace MarioBalance
 
         void DisableLaser()
         {
+            boxCollider.enabled = false;
             sprite.enabled = false;
         }
     }
