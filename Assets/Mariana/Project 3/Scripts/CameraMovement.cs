8efd9c0c1fe1db04ae86b5d18f81d40a2c10a44e
m-holtz <mholtz@newschool.edu>
2022-10-24 10:48:20 -0400

    fixed namespace

--- Assets/Mariana/Project 3/Scripts/CameraMovement.cs
+++ Assets/Mariana/Project 3/Scripts/CameraMovement.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
-
+namespace Mariana
 {
+    public class CameraMovement : MonoBehaviour
 
-    public Transform target;
-    // Start is called before the first frame update
-    void Start()
     {
-    }
+
+        public Transform target;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = new Vector3(transform.position.x, target.position.y, -10);
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y, -10);
 
+        }
     }
 }
