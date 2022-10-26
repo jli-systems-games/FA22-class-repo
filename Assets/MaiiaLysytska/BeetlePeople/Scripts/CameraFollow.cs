cf4c7b5bc11b5249883dbaff6faa8bd426101023
DinosaurPower <lysym685@newschool.edu>
2022-10-25 22:42:47 -0400

    sin of setting up camera

--- Assets/MaiiaLysytska/BeetlePeople/Scripts/CameraFollow.cs
+++ Assets/MaiiaLysytska/BeetlePeople/Scripts/CameraFollow.cs
@@ -26,7 +26,7 @@ public class CameraFollow : MonoBehaviour
         beetpos1 = beetle1.GetComponent<Transform>().position;
     beetpos2 = beetle2.GetComponent<Transform>().position;
         // How many units should we keep from the players
-        float zoomFactor = 1.5f;
+        float zoomFactor = 1.2f;
         float followTimeDelta = 0.8f;
  
         // Midpoint we're after
