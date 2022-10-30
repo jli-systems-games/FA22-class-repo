a83f600c957bce7bc6cdcd20dd79cd424c26bbfa
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-10-30 13:43:06 -0400

    Exit behavior modified

--- Assets/Ekaterina/Balance/Scripts/ColliderDisabled.cs
+++ Assets/Ekaterina/Balance/Scripts/ColliderDisabled.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,18 @@ namespace Ekaterina
 {
     public class ColliderDisabled : MonoBehaviour
     {
+        public TrailRenderer trail;
+        public ParticleSystemRenderer particles;
         public BoxCollider2D objectA;
-        BoxCollider2D player;
+        public BoxCollider2D exit;
         
+        BoxCollider2D player;
+
+        private void Awake()
+        {
+            exit.enabled = false;
+        }
+
         void OnCollisionEnter2D(Collision2D player)
         {
 
@@ -16,8 +26,23 @@ namespace Ekaterina
             {
                 objectA.enabled = false;
                 Debug.Log("Collision!");
-                
+
             }
+            
+            if (player.gameObject.name == "Exit_floor")
+            {
+                Debug.Log("Collision2!");
+                
+                exit.enabled = true;
+                
+                gameObject.GetComponent<Renderer> ().material.color = Color.black;
+                trail.GetComponent<TrailRenderer> ().enabled = false;
+                particles.GetComponent<ParticleSystemRenderer> ().enabled = false;
+
+            }
+
+
+
         }
     }
 }
