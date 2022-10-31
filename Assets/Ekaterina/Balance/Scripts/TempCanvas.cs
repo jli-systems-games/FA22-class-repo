6b7d14854ae3914fa9a47eb8e6adefb738affb3b
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-10-31 00:32:18 -0400

    Temp canvas added + final settings for the 2nd camera

---
+++ Assets/Ekaterina/Balance/Scripts/TempCanvas.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ekaterina
+{
+    public class TempCanvas : MonoBehaviour
+    {
+        public GameObject TempCanvas;
+        public BoxCollider2D player2;
+
+        void OnCollisionEnter2D(Collision2D player2)
+        {
+            if (player2.gameObject.name == "Block 2")
+            {
+                TempCanvas.SetActive(!TempCanvas.activeSelf);
+            }
+        }
+    }
+}
