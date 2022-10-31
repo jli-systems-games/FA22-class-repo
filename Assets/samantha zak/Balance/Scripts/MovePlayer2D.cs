e6dad86f3d38306b5ff1abbc73191ddd305eccdd
zaksaja <81482722+zaksaja@users.noreply.github.com>
2022-10-31 00:45:26 -0400

    Body

--- Assets/samantha zak/Balance/Scripts/MovePlayer2D.cs
+++ Assets/samantha zak/Balance/Scripts/MovePlayer2D.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class MovePlayer2D : MonoBehaviour
 {
-    private Rigidbody2D rigidBody2D;
-    [SerializeField] float moveSpeed = 2f;
-    // Start is called before the first frame update
+    //private Rigidbody2D rigidBody2D;
+    //[SerializeField] float moveSpeed = 2f;
+    /// Start is called before the first frame update
     void Start()
     {
-        Rigidbody2D = GetComponent<Rigidbody2D>();
+        //Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
