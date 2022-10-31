6bbc80c81822165689c878a12c39c58f6fe15a93
zaksaja <81482722+zaksaja@users.noreply.github.com>
2022-10-31 01:10:14 -0400

    Scripts

---
+++ Assets/samantha zak/Balance/Scripts/Ebehvaiour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ebehvaiour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E)) //right
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        
+        }
+        
+    }
+}
--- Assets/samantha zak/Balance/Scripts/MovePlayer2D.cs
+++ Assets/samantha zak/Balance/Scripts/MovePlayer2D.cs
@@ -7,14 +7,38 @@ public class MovePlayer2D : MonoBehaviour
     //private Rigidbody2D rigidBody2D;
     //[SerializeField] float moveSpeed = 2f;
     /// Start is called before the first frame update
+    public float moveSpeed = 5;
     void Start()
     {
         //Rigidbody2D = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.R)) //right
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
         
+        }
+        /*
+        else if (Input.GetKey(KeyCode.E)) //right
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        
+        }
+        
+
+        else if (Input.GetKey(KeyCode.W)) //left
+        {
+            transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.Q)) //left
+        {
+            transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
+
+        }*/
     }
 }
