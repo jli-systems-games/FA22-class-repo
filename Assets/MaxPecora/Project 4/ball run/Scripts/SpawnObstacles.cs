f2dda5bbea3b6e7f3d56d6d99e62872f8c3ee329
pecom291 <pecom291@newschool.edu>
2022-10-30 23:09:14 -0400

    A HUGE commit
    - unity was being buggy so had to manually reimport assets from my backup that weren't working for unknown reason
    - scripts added and fixed
    - visuals added
    - score counter added
    - movement fixed, ball is much more flushed out
    - power ups idea in progress
    - start and end menus exist for once!
    - game is actually highly more up to date then was being represented (I am sorry its been a horrendous week)
    - most assets here were test ideas ALL will be changed in the next 2 hours

---
+++ Assets/MaxPecora/Project 4/ball run/Scripts/SpawnObstacles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacles : MonoBehaviour {
+
+	public float timer = 0;
+	public float level = 1f;
+	public float gravityScale = 0.3f;
+
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= level) {
+			if (level > 0.25f) {
+				level -= 0.02f;
+			}
+			timer = 0;
+			int posX = Random.Range (-8, 8);
+			GameObject g = Instantiate (Resources.Load ("box"), new Vector3 (posX, 6, 0), Quaternion.identity) as GameObject;
+			g.GetComponent<Rigidbody2D> ().gravityScale = gravityScale;
+			gravityScale += 0.005f;
+		}
+	}
+}
