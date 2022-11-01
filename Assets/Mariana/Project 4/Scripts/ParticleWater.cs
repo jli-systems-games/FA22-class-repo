28649faac93f66c50ef82505cd43396de68e221a
m-holtz <mholtz@newschool.edu>
2022-11-01 02:26:43 -0400

    lvl1 sound

--- Assets/Mariana/Project 4/Scripts/ParticleWater.cs
+++ Assets/Mariana/Project 4/Scripts/ParticleWater.cs
@@ -7,8 +7,16 @@ namespace Mariana
 public class ParticleWater : MonoBehaviour
 {
 
+    public AudioClip splash;
+
+    void Start ()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = splash;
+    }
    private void OnCollisionEnter2D(Collision2D collision)
    {
+        GetComponent<AudioSource>().Play();
         GetComponent<ParticleSystem>().Play();
         Debug.Log("hit");
        // GetComponent<SpriteRenderer>().enabled = false;
