a72e110e2d23aa4f9a63172bcb7799734de681ab
m-holtz <mholtz@newschool.edu>
2022-11-01 03:14:45 -0400

    last scene
    YEY

--- Assets/Mariana/Project 4/Scripts/LostLvl1.cs
+++ Assets/Mariana/Project 4/Scripts/LostLvl1.cs
@@ -7,10 +7,12 @@ namespace Mariana
 {
 public class LostLvl1 : MonoBehaviour
 {
+    public int currentIndex;
+    
     void OnTriggerEnter2D(Collider2D other)
               {
                   if (other.gameObject.tag == "Player")
-                      SceneManager.LoadScene("Water");
+                      SceneManager.LoadScene("Water" + currentIndex);
               }
 }
 }
