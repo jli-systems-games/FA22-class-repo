a8c643153fb74e09d9c6cde855a0bc0ca3f7be50
Jason <jasonli1337@gmail.com>
2022-11-11 22:29:44 -0500

    Sam namespaces

--- Assets/samantha zak/Balance/Scripts/CameraFollow.cs
+++ Assets/samantha zak/Balance/Scripts/CameraFollow.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace SamBalance
+namespace samantha_zak.Balance.Scripts
 {
     public class CameraFollow : MonoBehaviour
     {
